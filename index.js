4aa3df590d7f31d2ec20642985b9c30a0e911687
ismail <yako_is@yahoo.fr>
2023-07-23 23:45:16 +0100

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+let names = ["Sam", "Ismail", "Yakoub"]
+function writeCards(array, event) {
+    const messages = [];
+    for (let i = 0; i < names.length; i++) {
+        messages.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`);
+    }
+    return messages;
+}
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number--);
+    }
+}
